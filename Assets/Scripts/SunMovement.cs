2107312b95d93e90703d6a94a2387b969e8c4641
MrVizious <mrvizious@gmail.com>
2023-06-05 11:30:05 +0200

    Simple sun moving

---
+++ Assets/Scripts/SunMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMovement : MonoBehaviour
+{
+    public float rotationalSpeed = 5f;
+    void Update()
+    {
+        transform.Rotate(transform.right, rotationalSpeed * Time.deltaTime);
+    }
+}
