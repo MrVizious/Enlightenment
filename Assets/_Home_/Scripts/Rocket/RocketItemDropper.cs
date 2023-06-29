ff0a7e94a529332c8c14a591aa74852757cb918f
MrVizious <mrvizious@gmail.com>
2023-06-29 12:42:03 +0200

    Rocket sound added

--- Assets/_Home_/Scripts/Rocket/RocketItemDropper.cs
+++ Assets/_Home_/Scripts/Rocket/RocketItemDropper.cs
@@ -5,19 +5,19 @@ using Cysharp.Threading.Tasks;
 
 public class RocketItemDropper : MonoBehaviour
 {
-    public async UniTask InitializeSequence(Vector3 initialPosition, Vector3 finalPosition, Quaternion rotation, GameObject itemToDrop, float secondsToDrop = 5f)
+    public async UniTask InitializeSequence(Vector3 initialPosition, Vector3 finalPosition, Quaternion rotation, GameObject itemToDrop, float secondsToDrop = 4f)
     {
         initialPosition = initialPosition + (initialPosition - finalPosition) * 5;
         transform.position = initialPosition;
         transform.rotation = rotation;
         await MoveRocket(initialPosition, finalPosition, secondsToDrop);
         Instantiate(itemToDrop, finalPosition, rotation);
-        await UniTask.Delay(500);
+        await UniTask.Delay(1000);
         await MoveRocket(finalPosition, initialPosition, secondsToDrop);
         Destroy(gameObject);
     }
 
-    public async UniTask MoveRocket(Vector3 from, Vector3 to, float secondsToDrop = 5f)
+    public async UniTask MoveRocket(Vector3 from, Vector3 to, float secondsToDrop = 4f)
     {
         float distance = Vector3.Distance(from, to);
         float speed = distance / secondsToDrop;
